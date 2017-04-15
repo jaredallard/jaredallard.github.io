fbe93559a9378b381b89bf5550f65fd7a57373a3
Jared Allard <jaredallard@outlook.com>
2017-04-14 17:23:26 -0700

    gulp: csso & sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 'use strict';
 
-const gulp = require('gulp'),
-      sass = require('gulp-sass');
+const gulp       = require('gulp'),
+      sass       = require('gulp-sass'),
+      sourcemaps = require('gulp-sourcemaps'),
+      minifyCSS  = require('gulp-csso');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+  return gulp.src('sass/main/*.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(minifyCSS())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/main'))
 });
 
 gulp.task('sass:watch', function () {
